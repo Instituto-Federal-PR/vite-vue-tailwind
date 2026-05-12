90e3aa30f634f533ab4f2b240858af256d8952c8
gileduardo <gilgea@gmail.com>
2026-05-12 20:51:01 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="min-h-screen bg-slate-900 flex items-center justify-center">
+    <h1 class="text-5xl font-bold text-sky-400 drop-shadow-lg">
+      Tailwind + Vite/Vue funcionando!
+    </h1>
+  </div>
+</template>
